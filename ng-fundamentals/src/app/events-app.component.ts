f0cd9ef8df20125fd20407f2f78d8af4ae15a2a7
vatansoni <vatansoni89@gmail.com>
2018-05-23 03:29:31 +0530

    Bootstrap changes

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<h2>Hello World</h2>
+  <img src="/assets/basic-shield.png"/>`
+  
+})
+export class EventsAppComponent {
+  title = 'app';
+}
